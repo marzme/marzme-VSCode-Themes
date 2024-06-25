096d1d9c078e1ce9f0fcb307260326c57e47d5b4
Marley <marleybarrett@hotmail.com>
2024-06-25 18:30:54 +1000

    Initial commit

---
+++ Convert-ISEThemeToVSCodeTheme/Convert-ISEThemeToVSCodeTheme.ps1
@@ -0,0 +1,297 @@
+# The below PowerShell commands can be used to convert ps1xml ISE theme files into VS Code JSON theme files
+
+# Define the path to where the ps1xml ISE theme files are stored
+$ISEThemesRootPath = "C:\Administrative"
+
+# Get the full file path for each ps1xml ISE theme file
+$ISEThemeFiles = Get-ChildItem -Path "$ISEThemesRootPath\*.ps1xml" | Select Fullname
+
+# Process each ps1xml ISE theme file
+foreach ($ISEThemeFilepath in $ISEThemeFiles) {
+
+    # Get the XML content of the ps1xml ISE theme file
+    [xml]$ISEThemeXML = Get-Content -Path $ISEThemeFilepath.FullName
+
+    # Extract the theme name from the ps1xml ISE theme file
+    $ThemeName = $ISEThemeXML.StorableColorTheme.Name
+    Write-Verbose "Converting $ThemeName..." -Verbose
+
+    # Initialise ISEThemeColourValuePairs custom object
+    $ISEThemeColourValuePairs = New-Object -TypeName PSObject
+
+    # Get a list of the XML keys in the ISE theme
+    $ISEThemeKeys = $ISEThemeXML.StorableColorTheme.Keys.GetEnumerator().'#Text'
+
+    # Extract the relevant name and colour value pairs from the XML ISE theme
+    for ($i = 0; $i -lt $ISEThemeKeys.Count; $i++) {
+
+        # Get current colour name
+        [string]$ExtractedColourName = ($ISEThemeXML.StorableColorTheme.Keys.GetEnumerator().'#Text')[$i]
+
+        # Check if it's a script pane colour as those are all we're looking for
+        if (($ExtractedColourName -notlike "Console*") -and ($ExtractedColourName -notlike "Xml*")) {
+
+            # If it's a TokenColors colour, trim the start of its name to avoid a slash in property name
+            if ($ExtractedColourName -like "TokenColors\*") {
+                $ColourName = $ExtractedColourName.Replace("TokenColors\","")
+            } else {
+                $ColourName = $ExtractedColourName
+            }
+
+            # Get the current RGB colour and convert it to a hex value
+            ($ISEThemeXML.StorableColorTheme.Values.GetEnumerator() | Select R, G, B)[$i] | foreach {
+
+                [int]$RIntValue = $_.R
+                [int]$GintValue = $_.G
+                [int]$BIntValue = $_.B
+
+                $RHexValue = "{0:x2}" -f $RIntValue
+                $GHexValue = "{0:x2}" -f $GintValue
+                $BHexValue = "{0:x2}" -f $BIntValue
+
+                [string]$CurrentColour = ("$($RHexValue)$($GHexValue)$($BHexValue)").ToUpper()
+
+
+                # Also get a slightly lighter version of the colour if possible
+                if ($RIntValue -lt 235) {$RHexValueLighter = "{0:x2}" -f ($RIntValue + 20)} else {$RHexValueLighter = "FF"}
+                if ($GIntValue -lt 235) {$GHexValueLighter = "{0:x2}" -f ($GIntValue + 20)} else {$GHexValueLighter = "FF"}
+                if ($BIntValue -lt 235) {$BHexValueLighter = "{0:x2}" -f ($BIntValue + 20)} else {$BHexValueLighter = "FF"}
+
+                [string]$CurrentColourLighter = ("$($RHexValueLighter)$($GHexValueLighter)$($BHexValueLighter)").ToUpper()
+
+
+                # Also get a slightly darker version of the colour if possible
+                if ($RIntValue -gt 20) {$RHexValueDarker = "{0:x2}" -f ($RIntValue - 20)} else {$RHexValueDarker = "00"}
+                if ($GIntValue -gt 20) {$GHexValueDarker = "{0:x2}" -f ($GIntValue - 20)} else {$GHexValueDarker = "00"}
+                if ($BIntValue -gt 20) {$BHexValueDarker = "{0:x2}" -f ($BIntValue - 20)} else {$BHexValueDarker = "00"}
+
+                [string]$CurrentColourDarker = ("$($RHexValueDarker)$($GHexValueDarker)$($BHexValueDarker)").ToUpper()
+
+                # Add colour name and hex value pairs to ISEThemeColourValuePairs custom object
+                $ISEThemeColourValuePairs | Add-Member -MemberType NoteProperty -Name $ColourName -Value $CurrentColour
+                $ISEThemeColourValuePairs | Add-Member -MemberType NoteProperty -Name "$($ColourName)Lighter" -Value $CurrentColourLighter
+                $ISEThemeColourValuePairs | Add-Member -MemberType NoteProperty -Name "$($ColourName)Darker" -Value $CurrentColourDarker
+
+            }# $ColourValue = ...
+
+        }# if (($ExtractedColourName...
+
+    }# for ($i = 0;...
+
+    # Construct the VS Code JSON theme
+    $VSCodeThemeJSON = @"
+{
+    "name": "$ThemeName",
+    "tokenColors": [
+        {
+            "name": "Comments",
+            "scope": [
+                "comment"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Comment)"
+            }
+        },
+        {
+            "name": "Comments: Preprocessor",
+            "scope": "comment.block.preprocessor",
+            "settings": {
+                "fontStyle": "",
+                "foreground": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColor)"
+            }
+        },
+        {
+            "name": "Comments: Documentation",
+            "scope": [
+                "comment.documentation",
+                "comment.block.documentation"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColor)"
+            }
+        },
+        {
+            "name": "Invalid - Deprecated",
+            "scope": "invalid.deprecated",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.WarningForegroundColor)"
+            }
+        },
+        {
+            "name": "Invalid - Illegal",
+            "scope": "invalid.illegal",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.WarningForegroundColor)"
+            }
+        },
+        {
+            "name": "Keywords",
+            "scope": [
+                "keyword.other",
+                "keyword.control",
+                "punctuation.section.group",
+                "punctuation.section.braces"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Keyword)"
+            }
+        },
+        {
+            "name": "Types",
+            "scope": [
+                "storage.type"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Type)"
+            }
+        },
+        {
+            "name": "Language Constants",
+            "scope": [
+                "constant.language",
+                "support.constant",
+                "variable.language"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Variable)"
+            }
+        },
+        {
+            "name": "Variables",
+            "scope": [
+                "variable",
+                "support.variable",
+                "punctuation.definition.variable"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Variable)"
+            }
+        },
+        {
+            "name": "Tag attribute",
+            "scope": [
+                "entity.other.attribute-name",
+                "variable.parameter",
+                "keyword.operator",
+                "meta"
+            ],
+            "settings": {
+                "fontStyle": "",
+                "foreground": "#$($ISEThemeColourValuePairs.Operator)"
+
+            }
+        },
+        {
+            "name": "Functions",
+            "scope": [
+                "entity.name.function"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Number)"
+            }
+        },
+        {
+            "name": "Functions2",
+            "scope": [
+                "support.function"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Command)"
+            }
+        },
+        {
+            "name": "Classes",
+            "scope": [
+                "entity.name.tag",
+                "entity.name.type",
+                "entity.other.inherited-class",
+                "support.class"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Keyword)"
+            }
+        },
+        {
+            "name": "Exceptions",
+            "scope": "entity.name.exception",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.ErrorForegroundColor)"
+            }
+        },
+        {
+            "name": "Sections",
+            "scope": "entity.name.section",
+            "settings": {
+            }
+        },
+        {
+            "name": "Numbers, Characters",
+            "scope": [
+                "constant.numeric",
+                "constant"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Number)"
+            }
+        },
+        {
+            "name": "User-defined constant",
+            "scope": [
+                "constant.character",
+                "constant.other"
+            ],
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.Keyword)"
+            }
+        },
+        {
+            "name": "Strings",
+            "scope": "string",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.String)"
+            }
+        },
+        {
+            "name": "Strings: Escape Sequences",
+            "scope": "constant.character.escape",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.String)"
+            }
+        },
+        {
+            "name": "Strings: Regular Expressions",
+            "scope": "string.regexp",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.String)"
+            }
+        },
+        {
+            "name": "Strings: Symbols",
+            "scope": "constant.other.symbol",
+            "settings": {
+                "foreground": "#$($ISEThemeColourValuePairs.String)"
+            }
+        }
+    ],
+    "colors": {
+        "titleBar.activeBackground": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColor)",
+        "titleBar.activeForeground": "#$($ISEThemeColourValuePairs.OperatorDarker)",
+		"tab.activeBackground": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColorDarker)",
+		"tab.activeForeground": "#$($ISEThemeColourValuePairs.OperatorLighter)",
+		"tab.inactiveBackground": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColorLighter)",
+		"tab.inactiveForeground": "#$($ISEThemeColourValuePairs.OperatorDarker)",
+		"editor.background": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColor)",
+        "editor.foreground": "#$($ISEThemeColourValuePairs.Operator)",
+		"activityBar.background": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColorLighter)",
+        "activityBar.foreground": "#$($ISEThemeColourValuePairs.Operator)",
+		"sideBar.background": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColorDarker)",
+		"statusBar.background": "#$($ISEThemeColourValuePairs.ScriptPaneBackgroundColorLighter)"
+    }
+}
+"@
+
+    # Save the resulting VS Code JSON theme file to the same directory as the source ps1xml file
+    $ThemeFileName = $ThemeName.Replace(" ","-")
+    $VSCodeThemeJSON | Out-File -FilePath "$ISEThemesRootPath\$ThemeFileName-color-theme.json" -Encoding utf8
+
+}# foreach ($ISEThemeFilepath in $ISEThemeFiles)...
--- README.md
+++ README.md
@@ -1,1 +1,167 @@
-a
+# marzme-VSCode-Themes
+A Visual Studio Code extension which contains a collection of themes.
+
+<br>
+
+## How to Use
+Download the entire repo, unzip it, and store it in "C:\Users\USERNAME\\.vscode\extensions" in a folder called marzme-vscode-themes. Then reload VS Code, go to Extensions in the sidebar, right-click on "marzme VSCode Themes" and click "Set Color Theme".
+
+At some point I'll probably put this up on the [VS Code Marketplace](https://marketplace.visualstudio.com/search?target=VSCode&category=Themes&sortBy=Installs) as a proper extension when I have time.
+
+<br>
+
+
+## Original Sources
+Most of these themes were converted from the ones on my [PowerShell_ISE_Themes](https://github.com/marzme/PowerShell_ISE_Themes) repo, using the PowerShell script in the Convert-ISEThemeToVSCodeTheme folder, so please refer to that ISE Themes repo for details crediting the origin of the themes.
+
+<br>
+
+## Example Screenshots
+#### Ambient:
+![Alt text](Example_Screenshots/Ambient.png?raw=true)
+<hr>
+
+#### Ambients:
+![Alt text](Example_Screenshots/Ambients.png?raw=true)
+<hr>
+
+#### Aquatic:
+![Alt text](Example_Screenshots/Aquatic.png?raw=true)
+<hr>
+
+#### Base16 Tomorrow:
+![Alt text](Example_Screenshots/Base16-Tomorrow.png?raw=true)
+<hr>
+
+#### Black Pastel:
+![Alt text](Example_Screenshots/Black-Pastel.png?raw=true)
+<hr>
+
+#### DependOnMe:
+![Alt text](Example_Screenshots/DependOnMe.png?raw=true)
+<hr>
+
+#### Dracula JetBrains:
+![Alt text](Example_Screenshots/Dracula-JetBrains.png?raw=true)
+<hr>
+
+#### Flare v1:
+![Alt text](Example_Screenshots/Flare-v1.png?raw=true)
+<hr>
+
+#### Gedit Original Oblivion:
+![Alt text](Example_Screenshots/Gedit-Original-Oblivion.png?raw=true)
+<hr>
+
+#### Havenjark:
+![Alt text](Example_Screenshots/Havenjark.png?raw=true)
+<hr>
+
+#### Monokai:
+![Alt text](Example_Screenshots/Monokai.png?raw=true)
+<hr>
+
+#### Navy Ocean:
+![Alt text](Example_Screenshots/Navy-Ocean.png?raw=true)
+<hr>
+
+#### Nightvale:
+![Alt text](Example_Screenshots/Nightvale.png?raw=true)
+<hr>
+
+#### Notepad++ Like:
+![Alt text](Example_Screenshots/Notepad++-Like.png?raw=true)
+<hr>
+
+#### Oblivion:
+![Alt text](Example_Screenshots/Oblivion.png?raw=true)
+<hr>
+
+#### Obsidian:
+![Alt text](Example_Screenshots/Obsidian.png?raw=true)
+<hr>
+
+#### Ocean Deep:
+![Alt text](Example_Screenshots/Ocean-Deep.png?raw=true)
+<hr>
+
+#### OneDark Pro:
+![Alt text](Example_Screenshots/OneDark-Pro.png?raw=true)
+<hr>
+
+#### Paper Rose:
+![Alt text](Example_Screenshots/Paper-Rose.png?raw=true)
+<hr>
+
+#### Pastel:
+![Alt text](Example_Screenshots/Pastel.png?raw=true)
+<hr>
+
+#### Placidity:
+![Alt text](Example_Screenshots/Placidity.png?raw=true)
+<hr>
+
+#### Plastic Code Wrap:
+![Alt text](Example_Screenshots/Plastic-Code-Wrap.png?raw=true)
+<hr>
+
+#### Python Idle Tango:
+![Alt text](Example_Screenshots/Python-Idle-Tango.png?raw=true)
+<hr>
+
+#### Railscast:
+![Alt text](Example_Screenshots/Railscast.png?raw=true)
+<hr>
+
+#### RecognEyes:
+![Alt text](Example_Screenshots/RecognEyes.png?raw=true)
+<hr>
+
+#### Retta:
+![Alt text](Example_Screenshots/Retta.png?raw=true)
+<hr>
+
+#### Roboticket:
+![Alt text](Example_Screenshots/Roboticket.png?raw=true)
+<hr>
+
+#### Solarized:
+![Alt text](Example_Screenshots/Solarized.png?raw=true)
+<hr>
+
+#### Space Gray:
+![Alt text](Example_Screenshots/Space-Gray.png?raw=true)
+<hr>
+
+#### Sublime Text 2:
+![Alt text](Example_Screenshots/Sublime-Text-2.png?raw=true)
+<hr>
+
+#### Tomorrow Night:
+![Alt text](Example_Screenshots/Tomorrow-Night.png?raw=true)
+<hr>
+
+#### Vibrant Ink:
+![Alt text](Example_Screenshots/Vibrant-Ink.png?raw=true)
+<hr>
+
+#### Vim Dark:
+![Alt text](Example_Screenshots/Vim-Dark.png?raw=true)
+<hr>
+
+#### Vim Dessert:
+![Alt text](Example_Screenshots/Vim-Dessert.png?raw=true)
+<hr>
+
+#### VS2013 Dark:
+![Alt text](Example_Screenshots/VS2013-Dark.png?raw=true)
+<hr>
+
+#### Wombat:
+![Alt text](Example_Screenshots/Wombat.png?raw=true)
+<hr>
+
+#### Zenburn:
+![Alt text](Example_Screenshots/Zenburn.png?raw=true)
+<hr>
+
